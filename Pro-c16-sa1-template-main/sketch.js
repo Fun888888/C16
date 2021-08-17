62dec6a9d79e209ec804ce086126caaccc66b89c
Fun888888 <88375601+Fun888888@users.noreply.github.com>
2021-08-17 16:01:40 +0800

    Add files via upload

---
+++ Pro-c16-sa1-template-main/sketch.js
@@ -0,0 +1,18 @@
+
+var box1;
+var box2;
+
+function setup() {
+  createCanvas(600, 400);
+ box1=new Box();
+ box2=new Box(); 
+}
+
+function draw() {
+  background(220);
+box1.show();
+box2.show();
+box1.setWidth(50);
+box2.setWidth(100);
+}
+
